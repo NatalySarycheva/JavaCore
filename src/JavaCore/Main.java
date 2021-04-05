055eb9c611eee8ec4f0360b1a6ce9776ec9c56fb
Nataly <bzi92@bk.ru>
2021-04-05 22:51:29 +0300

    lesson_5

--- src/JavaCore/Main.java
+++ src/JavaCore/Main.java
@@ -1,8 +1,12 @@
 package JavaCore;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        AppData appData = new AppData();
+        appData.save("appData.csv");
+        appData.load("appData.csv");
     }
 }
